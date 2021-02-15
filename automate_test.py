d3f496c3773ce2c17c0b4164c90e2e659a466552
Em <dcshi6@gmail.com>
2021-02-14 20:22:27 -0500

    Update automate_test.py

--- automate_test.py
+++ automate_test.py
@@ -82,3 +82,44 @@ def automate_test():
         f.close()
 
         return 0
+
+    elif m == "F" or m == "f" or m == "fire":
+        flame = input("Enter a flammability value from a range of 0 to 1: ")
+        m = input("Which fire strategy should I use (pick a number 1 to 3), or quit to stop: ")
+        num_attempts = input("How many attempts? ")
+        file_name = input(
+            "Input the name of the file you want to store the output: ")
+        orig_stdout = sys.stdout
+        f = open(file_name, "w")
+        sys.stdout = f
+
+        num_success = 0
+        print("AUTOMATED TEST RESULT DATA")
+        print("Credits: Siddhi Kasera and Em Shi")
+        print("Dimension of:", dim)
+        print("Density:", p)
+        print("Flammability", flame)
+        print("Strategy", m)
+        print("--------------")
+        for i in range(int(num_attempts)):
+            print("\nAttempt ", i)
+            if m == "1" or m == "Strategy 1":
+                arr = generate_array(int(dim), float(p))
+                t0 = time.time()
+                if strategy1(arr, int(dim), float(flame)):
+                    print(time.time() - t0, "seconds")
+                    num_success = num_success+1
+
+            elif m == "2" or m == "Strategy 2":
+                arr = generate_array(int(dim), float(p))
+                t0 = time.time()
+                if strategy2(arr, int(dim), float(flame)):
+                    print(time.time() - t0, "seconds")
+                    num_success = num_success+1
+
+        print("\n----OVERALL:----")
+        print(num_success, "succeeded")
+        print("out of", num_attempts, "attempts")
+        # end file output
+        sys.stdout = orig_stdout
+        f.close()
