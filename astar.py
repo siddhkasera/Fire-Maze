61f2f0fd3ef9ac6aa2e01154eee81fc50bb8bbc6
siddhi <smk339@scarletmail.rutgers.edu>
2021-02-19 23:10:56 +0530

    astar clean and comment, auto test changes, plot

--- astar.py
+++ astar.py
@@ -13,16 +13,7 @@ class AstarNode:
         self.pt = pt
         self.dist = dist
         self.g = 0
-        self.h = 0
         self.f = 0
-
-    # def __eq__(self, other):
-        # return (self.pt.x == other.x) and (self.pt.y == other.y)
-      #  return self.pt == other.pt
-
-    # def __str__(self):
-        # return f'x is {self.pt.x} and y is {self.pt.y}'
-      #  return f'x is {self.pt.x} and y is {self.pt.y}'
 
     def __lt__(self, other):
         return self.f < other.f
@@ -38,28 +29,22 @@ def isValid(arr, dim, row, col):
         return True
 
 
-def reconstruct_path(arr, came_from, current):
-    while current in came_from:
-        print(came_from)
-        current = came_from[current]
-        i = current.x
-        j = current.y
-        arr[i][j] = "V"
+def reconstruct_path(arr, dim, came_from):
+    for i in came_from:
+        if (i.x != 0 and i.x != 0) or (i.x != dim and i.x != dim):
+            arr[i.x][i.y] = "V"
     return arr
 
 
 def heuristic(a, b):
     """ Calculates the Euclidean Distance between two co-ordinate points """
-    print("values in a and b inside heuristic are:")
-    print(a.x, a.y)
-    print(b.x, b.y)
     z = sqrt(abs(a.x - b.x) ** 2) + sqrt(abs((a.y - b.y) ** 2))
-    print("H value is")
-    print(z)
+
     return z
 
 
 def astar(arr, dim):
+    # direction array
     dx = [-1, 0, 0, 1]
     dy = [0, -1, 1, 0]
 
@@ -69,7 +54,56 @@ def astar(arr, dim):
     visited = [[False for i in range(dim)] for j in range(dim)]
     visited[0][0] = True
 
-    src.g = src.h = src.f = 0
+    # setting the g and f values to 0
+    src.g = src.f = 0
+
+    open_list = []
+    closed_list = []
+    came_from = {}
+    s = AstarNode(src, 0)
+    heapq.heapify(open_list)
+    heapq.heappush(open_list, s)
+
+    while len(open_list) > 0:
+        current = heapq.heappop(open_list)
+        closed_list.append(current)
+        pt = current.pt
+        if pt.x == dest.x and pt.y == dest.y:  # reached the destination
+            print("A* distance: " + str(current.dist))
+            return reconstruct_path(arr, dim, came_from)
+
+        for i in range(4):
+            row = pt.x + dx[i]
+            col = pt.y + dy[i]
+            if isValid(arr, dim, row, col) and arr[row][col] != 'X' and (not visited[row][col]):
+                new_node = AstarNode(row, col)
+                neighbor = Point(row, col)
+                tent_g = current.g + 1  # calculate tentative g value
+                if new_node in closed_list and tent_g >= new_node.g:
+                    continue
+                if tent_g < new_node.g or new_node not in open_list:
+                    came_from[neighbor] = current
+                    new_node.g = tent_g
+                    new_node.f = tent_g + heuristic(neighbor, dest)
+                    new_node.pt = Point(row, col)
+                    # pushing the new node in the heap
+                    heapq.heappush(open_list, new_node)
+
+    print("Astar failed")
+    return arr
+
+
+def auto_astar(arr, dim):
+    dx = [-1, 0, 0, 1]
+    dy = [0, -1, 1, 0]
+
+    src = Point(0, 0)
+    dest = Point(dim - 1, dim - 1)
+
+    visited = [[False for i in range(dim)] for j in range(dim)]
+    visited[0][0] = True
+
+    src.g = src.f = 0
 
     open_list = []
     closed_list = []
@@ -83,17 +117,14 @@ def astar(arr, dim):
         closed_list.append(current)
         pt = current.pt
         if pt.x == dest.x and pt.y == dest.y:
-            return reconstruct_path(arr, current, came_from)
-
-        neighbor = []
+            return current.dist
 
         for i in range(4):
             row = pt.x + dx[i]
             col = pt.y + dy[i]
             if isValid(arr, dim, row, col) and arr[row][col] != 'X' and (not visited[row][col]):
                 new_node = AstarNode(row, col)
-                neighbor = Point(row,col)
-                # print(new_node.__str__())
+                neighbor = Point(row, col)
                 tent_g = current.g + 1
                 if new_node in closed_list and tent_g >= new_node.g:
                     continue
@@ -101,11 +132,8 @@ def astar(arr, dim):
                     came_from[neighbor] = current
                     new_node.g = tent_g
                     new_node.f = tent_g + heuristic(neighbor, dest)
-                    open_list.append(new_node)
+                    new_node.pt = Point(row, col)
                     heapq.heappush(open_list, new_node)
 
-        # if current != src:
-            # closed_list.add(current)
-
     print("Astar failed")
-    return arr
+    return -1
--- automate_test.py
+++ automate_test.py
@@ -1,7 +1,8 @@
+
 from generate_array import generate_array
-from auto_bfs import *
-from auto_dfs import *
-from auto_astar import *
+from bfs_algo import *
+from dfs_algo import *
+from astar import *
 from fire import *
 import time
 import sys
@@ -23,6 +24,7 @@ def automate_test():
 
         num_success = 0
         total_time = 0
+        total_dist = 0
         orig_stdout = sys.stdout
         f = open(file_name, "w")
         sys.stdout = f
@@ -41,6 +43,7 @@ def automate_test():
                 if r > -1:
                     print(r, "steps")
                     num_success = num_success + 1
+                    total_dist = total_dist+r
                 print(time.time() - t0, "seconds")
                 total_time = total_time + time.time() - t0
 
@@ -51,6 +54,7 @@ def automate_test():
                 if r > -1:
                     print(r, "steps")
                     num_success = num_success + 1
+                    total_dist = total_dist + r
                 print(time.time() - t0, "seconds")
                 total_time = total_time + time.time() - t0
 
@@ -61,6 +65,7 @@ def automate_test():
                 if r > -1:
                     print(r, "steps")
                     num_success = num_success + 1
+                    total_dist = total_dist + r
                 print(time.time() - t0, "seconds")
                 total_time = total_time + time.time() - t0
 
@@ -68,16 +73,69 @@ def automate_test():
         print(num_success, "succeeded")
         print("out of", num_attempts, "attempts")
         num_attem = int(num_attempts)
+        avg_time = 0
         if num_success > 0:
-            avg_attempts = num_attem / num_success
-            print("The average num of attempts ", avg_attempts)
+            avg_time = total_time / num_success
         else:
-            print("Average on attempts is 0")
-        print("total time:", total_time)
-        avg_time = total_time / num_attem
-        print("The average time taken", avg_time, "seconds")
+            print("Average distance is 0")
+
+        avg_dist = 0
+        print("The average time taken:", avg_time, "seconds")
+        if(num_success > 0):
+            avg_dist = total_dist / num_success
+        else:
+            print("Average time is 0 since")
+        print("The average distance used:", avg_dist, "units")
         # end file output
         sys.stdout = orig_stdout
         f.close()
 
         return 0
+
+    elif m == "F" or m == "f" or m == "fire":
+        flame = input("Enter a flammability value from a range of 0 to 1: ")
+        m = input(
+            "Which fire strategy should I use (pick a number 1 to 3), or quit to stop: ")
+        num_attempts = input("How many attempts? ")
+        file_name = input(
+            "Input the name of the file you want to store the output: ")
+        orig_stdout = sys.stdout
+        f = open(file_name, "w")
+        sys.stdout = f
+
+        num_success = 0
+        print("AUTOMATED TEST RESULT DATA")
+        print("Credits: Siddhi Kasera and Em Shi")
+        print("Dimension of:", dim)
+        print("Density:", p)
+        print("Flammability", flame)
+        print("Strategy", m)
+        print("--------------")
+        for i in range(int(num_attempts)):
+            print("\nAttempt ", i)
+            if m == "1" or m == "Strategy 1":
+                while True:
+                    arr = generate_array(int(dim), float(p))
+                    if help_dfs(arr, int(dim)) == True:
+                        break
+                t0 = time.time()
+                if strategy1(arr, int(dim), float(flame)):
+                    print(time.time() - t0, "seconds")
+                    num_success = num_success+1
+
+            elif m == "2" or m == "Strategy 2":
+                while True:
+                    arr = generate_array(int(dim), float(p))
+                    if help_dfs(arr, int(dim)) == True:
+                        break
+                t0 = time.time()
+                if strategy2(arr, int(dim), float(flame)):
+                    print(time.time() - t0, "seconds")
+                    num_success = num_success+1
+
+        print("\n----OVERALL:----")
+        print(num_success, "succeeded")
+        print("out of", num_attempts, "attempts")
+        # end file output
+        sys.stdout = orig_stdout
+        f.close()
